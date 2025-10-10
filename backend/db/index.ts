a4edd2386b9b6b821546ba1c1b507c8ce6e0e60e
Brandon Day <brandontday7@gmail.com>
2025-10-09 21:42:43 -0400

    basic db methods

--- backend/db/index.ts
+++ backend/db/index.ts
@@ -1,17 +1,19 @@
 import { Db, MongoClient } from "mongodb";
 import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER } from "../config.ts";
+import type { Playable } from "./schema.ts";
 
 const URI = `${DB_HOST}://${DB_USER}:${DB_PASSWORD}@juke-cluster.hiuzhwi.mongodb.net/?retryWrites=true&w=majority&appName=juke-cluster`;
-let CLIENT: MongoClient | undefined = undefined;
-let DB: Db | undefined = undefined;
+const CATALOGS = "catalogs";
+let client: MongoClient | undefined = undefined;
+let db: Db | undefined = undefined;
 
 const connect = async () => {
-  CLIENT = new MongoClient(URI);
+  client = new MongoClient(URI);
 
   try {
     console.log("Connecting to database...");
-    await CLIENT.connect();
-    DB = CLIENT.db(DB_NAME);
+    await client.connect();
+    db = client.db(DB_NAME);
     console.log("Connected to database!");
   } catch (e) {
     console.error(e);
@@ -19,17 +21,58 @@ const connect = async () => {
 };
 
 export const disconnect = async () => {
-  if (CLIENT) {
-    await CLIENT.close();
+  if (client) {
+    await client.close();
     console.log("Disconnected from database!");
   }
 };
 
 export const printCatalogs = async () => {
-  if (DB) {
-    const catalogs = await DB.collection("catalogs").find().toArray();
+  if (db) {
+    const catalogs = await db.collection("catalogs").find().toArray();
     console.log("Catalogs: ", catalogs);
   }
 };
+
+// export const printCatalog = async (title: string) => {
+//   if (db) {
+//     const catalog = db.collection("catalogs").find({ title });
+//     console.log("******", catalog);
+//   }
+// };
+
+export const createOrUpdateCatalog = async (
+  title: string,
+  playables: Playable[]
+) => {
+  if (db) {
+    const catalogs = db.collection(CATALOGS);
+
+    try {
+      await catalogs.updateOne(
+        { title },
+        { $set: { playables } },
+        {
+          upsert: true,
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
+
+// export const insertPlayable = async (
+//   title: string,
+//   playable: Playable,
+//   index?: number
+// ) => null;
+
+// export const removeFromCatalog = async (
+//   catalogTitle: string,
+//   playableTitle: string
+// ) => null;
+
+// export const removeCatalog = async (title: string) => null;
 
 export default connect;
--- backend/db/schema.ts
+++ backend/db/schema.ts
@@ -15,3 +15,12 @@ export interface Library {
   title: string;
   catalogs: Catalog[];
 }
+
+export const MAC = {
+  type: "album" as const,
+  title: "This Old Dog",
+  artistName: "Mac Demarco",
+  artworkUrl:
+    "https://i.scdn.co/image/ab67616d00001e029f1b21f21b13ff2d3e891f6b",
+  spId: "4NNq2vwTapv4fSJcrZbPH7",
+};
