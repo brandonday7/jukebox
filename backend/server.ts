da5f0225d631d13931c66fc2ab39b968ce9ee06b
Brandon Day <brandontday7@gmail.com>
2025-10-13 09:42:39 -0400

    started api

--- backend/server.ts
+++ backend/server.ts
@@ -1,16 +1,36 @@
 import express from "express";
-import { connect, disconnect } from "./db/index.ts";
+import {
+  connect,
+  createOrUpdateVibe,
+  disconnect,
+  findVibe,
+  findVibes,
+} from "./db/index.ts";
 import { PORT } from "./config.ts";
 
 const APP = express();
 
-APP.get("/", (req, res) => {
-  res.send("Welcome to my server!");
-});
-
 APP.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+APP.get("/vibes", async (_req, res) => {
+  const vibes = await findVibes();
+  res.send(vibes);
+});
+
+APP.get("/vibe/:title", async (req, res) => {
+  const vibe = await findVibe(req.params.title);
+  res.send(vibe);
+});
+
+// APP.post("/vibe/:title", async (req, res) => {
+//   const title = req.params.title;
+//   const playables = JSON.parse(req.query.playables as string);
+//   const hidden = req.query.hidden === "true";
+//   const vibe = await createOrUpdateVibe(title, playables, hidden);
+//   res.send(vibe);
+// });
 
 await connect();
 
